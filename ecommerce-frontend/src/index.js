ed7b4772b5ba03480bfc17d0d6e7a35e83560bd9
Tranduong03 <106472477+Tranduong03@users.noreply.github.com>
2025-09-02 22:43:34 +0700

    tàm tạm...

--- ecommerce-frontend/src/index.js
+++ ecommerce-frontend/src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// MY ADD
+import 'antd/dist/reset.css';
+
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+// END MY ADD
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
