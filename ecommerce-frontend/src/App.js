0417fdfd72f7481bdfa220c65642e74ac2bd8fb0
Tranduong03 <106472477+Tranduong03@users.noreply.github.com>
2025-09-02 22:29:13 +0700

    init prj

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello Admin</h1>
+      <Button type="dashed">Click me</Button>
+      <Button type="default">Click me</Button>
+      <Button type="link">Click me</Button>
+      <Button type="text">Click me</Button>
+      <Button type="primary">Click me</Button>
+
     </div>
   );
 }
